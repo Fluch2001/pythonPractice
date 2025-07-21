e7cb922a4875dfbcd85ceefb3bcc4785142be20f
Fluch2001 <fluchtenberg14@gmail.com>
2025-07-21 19:26:59 -0300

    Proyecto sentimientos openai

---
+++ pythonEjerciciosPractica/SOLID/ejercicio_complejo.py
@@ -0,0 +1,45 @@
+# #Desarrollar un chatbot en python que nos solicite algo y tome eso que le decimos, lo analice y nos responda cual es el sentimiento.
+
+# Este proyecto busca aplicar varios aspectos, POO, módulos, API, analisis de datos, etc.
+import openai
+
+openai.api_key = "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"
+system_rol = '''Vas a simular ser un analizador de sentimientos. Voy a escribirte mensajes y quiero que lo analices y me des una respuesta con al menos 1 caracter y maximo 4 caracteres, solo respuestas numericas. donde -1 es negatividad maxima y 1 es positividad maxima. 0 es neutral. (Podes responder solo con int o float)'''
+
+mensajes = [{"role": "system", "content": system_rol}]
+
+class Analizador_de_sentimientos:
+    def analizar_sentimiento(self, polaridad):
+        if polaridad <= -0.6:
+            return "\x1b[1;31m" + "Muy negativo"
+        elif polaridad <= -0.3:
+            return "\x1b[1;31m" + "Negativo"
+        elif polaridad < -0.1:
+            return "\x1b[1;31m" + "Algo negativo"
+        elif -0.1 <= polaridad <= 0.1:
+            return "\x1b[1;33m" + "Neutral"
+        elif polaridad < 0.4:
+            return "\x1b[1;32m" + "Algo positivo"
+        elif polaridad <= 0.9:
+            return "\x1b[1;32m" + "Positivo"
+        else:
+            return "\x1b[1;32m" + "Muy positivo"
+
+
+        
+analizador = Analizador_de_sentimientos()
+
+while True:
+    user_prompt = input("\x1b[1;33m" + "\n Decime algo: " + "\x1b[0;37m")
+    mensajes.append({"role": "user", "content": user_prompt})
+
+    completion = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = mensajes,
+        max_tokens = 10
+    )
+    respuesta = completion.choices[0].message["content"]
+    mensajes.append({"role": "assistant", "content": respuesta})
+
+    sentimiento = analizador.analizar_sentimiento(float(respuesta))
+    print(sentimiento)
